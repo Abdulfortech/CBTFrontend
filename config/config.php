d11b6be4e4f587dc3f8993218b309281d771ba67
abdulfortech <ibnmudi@gmail.com>
2025-04-18 13:25:54 +0100

    config

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'app_name' => 'Naija CBT',
+    'app_env' => 'local',
+    'app_url' => 'http://localhost/cbt-system',
+
+    'db_host' => 'localhost',
+    'db_name' => 'cbt',
+    'db_user' => 'root',
+    'db_pass' => '',
+
+    'timezone' => 'Africa/Lagos',
+
+    'secret_key' => 'someRandomSecureKey123',
+    'admin_email' => 'admin@naijacbt.com',
+];
