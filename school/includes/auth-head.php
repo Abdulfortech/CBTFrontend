6901abb48d49e8ad62044764cda6c3be2f6fb703
abdulfortech <ibnmudi@gmail.com>
2025-05-07 16:10:14 +0100

    added includes for auth files

--- school/includes/auth-head.php
+++ school/includes/auth-head.php
@@ -18,13 +18,14 @@
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
   <!-- Vendor CSS Files -->
-  <link href="../public/vendors/bootstrap-5.3.3/css/bootstrap.min.css" rel="stylesheet">
-  <link href="../vendor/twbs/bootstrap-icons/font/bootstrap-icons.min.css" rel="stylesheet">
+  <link href="../../public/vendors/bootstrap-5.3.3/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../../vendor/twbs/bootstrap-icons/font/bootstrap-icons.min.css" rel="stylesheet">
   <!-- Font awesome 6 -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet" type="text/css">
   <!-- DataTables  -->
-  <link href="../public/vendors/DataTables/datatables.min.css" rel="stylesheet" />
+  <link href="../../public/vendors/DataTables/datatables.min.css" rel="stylesheet" />
   <!-- Template Main CSS File -->
-  <link href="../public/assets/css/style.css" rel="stylesheet">
-  <link rel="stylesheet" href="../public/assets/css/custome.css">
+  <link href="../../public/assets/css/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="../../public/assets/css/custome.css">
+  <link rel="stylesheet" href="../../public/assets/css/toastr.css">
 </head>
--- school/includes/bottom.php
+++ school/includes/bottom.php
@@ -2,7 +2,7 @@
 
   <!-- Vendor JS Files -->
   <!-- <script src="assets/vendor/apexcharts/apexcharts.min.js"></script> -->
-  <script src="../../public/vendors/bootstrap-5.3.3/js/bootstrap.bundle.min.js"></script>
-  <script src="../../public/vendors/DataTables/datatables.min.js"></script>
+  <script src="../public/vendors/bootstrap-5.3.3/js/bootstrap.bundle.min.js"></script>
+  <script src="../public/vendors/DataTables/datatables.min.js"></script>
   <!-- Template Main JS File -->
-  <script src="../../public/assets/js/main.js"></script>
+  <script src="../public/assets/js/main.js"></script>
